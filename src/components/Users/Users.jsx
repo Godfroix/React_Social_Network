157830510f13b7a1ab2f639485e3055d69bb15f9
Godfroix <55955888+Godfroix@users.noreply.github.com>
2019-11-20 19:10:38 +0300

    v1.10

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,34 +1,65 @@
 import React from 'react';
 import usersCss from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png'
 
-const Users = (props) => {
-    return (
-        <div >
-            {
-                props.users.map((u) => <div key={u.id} className={usersCss.user}>
-                    <div className={usersCss.avaBtn}>
-                        <div>
-                            <img src={u.photoUrl} className={usersCss.avatar} />
+class Users extends React.Component {
+
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    getUsers = () => {
+        if (this.props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    this.props.setUsers(response.data.items);
+                });
+        };
+    };
+
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
+
+    render() {
+        return (
+            <div >
+                {/* <button onClick={this.getUsers} className={usersCss.usersBtn}>Get Users</button> */}
+                {
+                    this.props.users.map((u) => <div key={u.id} className={usersCss.user}>
+                        <div className={usersCss.avaBtn}>
+                            <div>
+                                <img src={u.photos.small != null ? u.photos.small : userPhoto} className={usersCss.avatar} />
+                                {/* <img src={u.photoUrl} className={usersCss.avatar} /> */}
+                            </div>
+                            <div>
+                                {u.followed
+                                    ? <button onClick={() => this.props.unfollow(u.id)} className={usersCss.btnun} >Unfollow</button>
+                                    : <button onClick={() => this.props.follow(u.id)} className={usersCss.btn}>Follow</button>}
+                            </div>
                         </div>
-                        <div>
-                            {u.followed
-                                ? <button onClick={() => props.unfollow(u.id)} className={usersCss.btnun} >Unfollow</button>
-                                : <button onClick={() => props.follow(u.id)} className={usersCss.btn}>Follow</button>}
+                        <div className={usersCss.info}>
+                            <div className={usersCss.nameStatus}>
+                                <div className={usersCss.name}>{u.name}</div>
+                                {/* <div className={usersCss.name}>{u.fullName}</div> */}
+                                <div className={usersCss.status}>{u.status}</div>
+                            </div>
+                            <div className={usersCss.countryCity}>
+                                <div className={usersCss.country}>{'u.location.country'}</div>
+                                <div className={usersCss.city}>{'u.location.city'}</div>
+                            </div>
                         </div>
-                    </div>
-                    <div className={usersCss.info}>
-                        <div className={usersCss.nameStatus}>
-                            <div className={usersCss.name}>{u.fullName}</div>
-                            <div className={usersCss.status}>{u.status}</div>
-                        </div>
-                        <div className={usersCss.countryCity}>
-                            <div className={usersCss.country}>{u.location.country}</div>
-                            <div className={usersCss.city}>{u.location.city}</div>
-                        </div>
-                    </div>
-                </div>)
-            }
-        </div>
-    )
-}
+                    </div>)
+                }
+            </div>
+        )
+    }
+};
+
+
+
 export default Users;
--- src/static/users_reducer.js
+++ src/static/users_reducer.js
@@ -7,26 +7,46 @@ const SET_USERS = 'SET-USERS';
 let initialState = {
     users:
         [
-            {
-                id: 1, followed: true, photoUrl: 'https://pbs.twimg.com/profile_images/1060286056342474752/FyIwsk1S.jpg',
-                fullName: 'Vitalii King', status: 'Я вернулся из небытия', location: { city: 'Venecia', country: 'Italy' }
-            },
-            {
-                id: 2, followed: true, photoUrl: 'https://lh5.googleusercontent.com/-QTUUAiSPNHA/AAAAAAAAAAI/AAAAAAAAAXk/khjvc_3VjUc/photo.jpg?sz=200',
-                fullName: 'Valera Gladiator', status: 'DA DA DA', location: { city: 'Samara', country: 'Russia' }
-            },
-            {
-                id: 3, followed: true, photoUrl: 'https://svirtus.cdnvideo.ru/40aqd88rYrAtKYtkWaM3SZlodIo=/0x0:1000x1000/200x200/filters:quality(100)/https://hb.bizmrg.com/esports-core-media/7a/7aa784cd39ed549ddf7602bd183cec0f.jpg?m=0d76aa7dbc5cdb4090eefa58f8cd710c',
-                fullName: 'Pukich', status: 'Говорите не молчите', location: { city: 'Tver', country: 'Russia' }
-            },
-            {
-                id: 4, followed: false, photoUrl: 'https://kibercon.ru/wp-content/uploads/2019/04/iner.jpg',
-                fullName: 'Guchibass', status: 'aaaaaaa', location: { city: 'Vinnica', country: 'Ukrain' }
-            },
-            {
-                id: 5, followed: false, photoUrl: 'https://www.b17.ru/foto/article/124863.jpg',
-                fullName: 'Alexander Borisov', status: 'Я спростмен', location: { city: 'Nizhnii-Novgorod', country: 'Russia' }
-            },
+            // {
+            //     id: 1,
+            //     followed: true,
+            //     photoUrl: 'https://pbs.twimg.com/profile_images/1060286056342474752/FyIwsk1S.jpg',
+            //     fullName: 'Vitalii King',
+            //     status: 'Я вернулся из небытия',
+            //     location: { city: 'Venecia', country: 'Italy' }
+            // },
+            // {
+            //     id: 2,
+            //     followed: true,
+            //     photoUrl: 'https://lh5.googleusercontent.com/-QTUUAiSPNHA/AAAAAAAAAAI/AAAAAAAAAXk/khjvc_3VjUc/photo.jpg?sz=200',
+            //     fullName: 'Valera Gladiator',
+            //     status: 'DA DA DA',
+            //     location: { city: 'Samara', country: 'Russia' }
+            // },
+            // {
+            //     id: 3,
+            //     followed: true,
+            //     photoUrl: 'https://svirtus.cdnvideo.ru/40aqd88rYrAtKYtkWaM3SZlodIo=/0x0:1000x1000/200x200/filters:quality(100)/https://hb.bizmrg.com/esports-core-media/7a/7aa784cd39ed549ddf7602bd183cec0f.jpg?m=0d76aa7dbc5cdb4090eefa58f8cd710c',
+            //     fullName: 'Pukich',
+            //     status: 'Говорите не молчите',
+            //     location: { city: 'Tver', country: 'Russia' }
+            // },
+            // {
+            //     id: 4,
+            //     followed: false,
+            //     photoUrl: 'https://kibercon.ru/wp-content/uploads/2019/04/iner.jpg',
+            //     fullName: 'Guchibass',
+            //     status: 'aaaaaaa',
+            //     location: { city: 'Vinnica', country: 'Ukrain' }
+            // },
+            // {
+            //     id: 5,
+            //     followed: false,
+            //     photoUrl: 'https://www.b17.ru/foto/article/124863.jpg',
+            //     fullName: 'Alexander Borisov',
+            //     status: 'Я спростмен',
+            //     location: { city: 'Nizhnii-Novgorod', country: 'Russia' }
+            // },
         ],
 
 };
